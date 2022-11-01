b22adf603a4992d435ad6a895d26e7b05f8f3b82
Leelai Hayslett <Leelai.Hayslett@gmail.com>
2022-11-01 19:30:19 -0400

    thought about the functions needed

---
+++ script.js
@@ -0,0 +1,22 @@
+// onkeydown function
+
+//display value function needed
+function displayResult(){
+
+}
+//function that takes value input by user and returns final result to display
+function quickMaths(){
+    
+}
+//function that does the math and returns the result on the screen
+// if else for every eventlistner
+// onkeydown function
+function buttonClicked(event){
+    alert ("Click activated");
+}
+
+//function that clears the display
+
+function clrBtn(){
+
+}
